7bc30e2d5ee6684f99c5e5216cd43fd3f693bc5a
suncomeon <mizhihua@163.com>
2014-08-19 23:35:28 +0800

    111

--- HelloBoy/src/com/helloboy/HelloBoy.java
+++ HelloBoy/src/com/helloboy/HelloBoy.java
@@ -3,7 +3,6 @@ package com.helloboy;
 public class HelloBoy {
 
 	public static void main(String args[]){
-		System.out.println("Hello GitHub...");
+		System.out.println("Hello GitHub..."); 
 	}
-	
 }
